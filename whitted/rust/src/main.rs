e647d429e6bc5e6097b7e30f57aa632797793785
embracetheoid <embracethevoid@github.com>
2018-11-18 11:18:20 -0800

    lifetime problem

--- whitted/rust/src/main.rs
+++ whitted/rust/src/main.rs
@@ -4,29 +4,40 @@ use geometry::objects::*;
 use geometry::vector::*;
 
 use std::f64::INFINITY;
-static MAX_DEPTH:u8 = 5;
+static MAX_DEPTH: u8 = 5;
 
-fn trace<'a>(ray_origin:&Vec3f , ray_direction:&Vec3f , objects:&Vec<Box<Object>>, tnear &mut f64) -> Option<&'a Box<Object>>{
-    if depth > MAX_DEPTH{
-        return None;
-    }
-    let closest_object:Option<&Object> = None;
-    for object in objects.iter(){
+fn trace<'a>(
+    ray_origin: &Vec3f,
+    ray_direction: &Vec3f,
+    objects: Vec<&'a Object>,
+    tnear: &mut f64,
+    index: &mut usize,
+    uv: &mut Vec2f,
+) -> Option<&'a Object> {
+    let mut closest_object: Option<&Object> = None;
+    for object in objects.iter() {
         let mut tmp_near = INFINITY;
-        if object.intersect(ray_origin, ray_direction,&mut tmp_near){
-            if tmp_near < tnear{
+        if object.intersect(ray_origin, ray_direction, &mut tmp_near, index, uv) {
+            if tmp_near < *tnear {
                 *tnear = tmp_near;
-                closest_object = Some(&object);
+                closest_object = Some(*object);
             }
         }
     }
     closest_object
 }
 
-fn cast_ray(ray_origin:&Vec3f , ray_direction:&Vec3f , objects:&Vec<Box<Object>>, tnear &mut f64){
-
+fn cast_ray<'a>(
+    ray_origin: &Vec3f,
+    ray_direction: &Vec3f,
+    objects: &Vec<&'a Object>,
+    tnear: &mut f64,
+    depth: u32,
+) {
+    if (depth > MAX_DEPTH) {
+        return background_color;
+    }
 }
-
 
 fn main() {
     // let t = Object::Triangle;
@@ -49,25 +60,36 @@ fn main() {
         },
     };
     let s = Sphere {
-            center: Vec3f{x:0.0,y:0.0,z:0.0},
-    radius: 2.0,
-    surface_color: Vec3f{x:1.0,y:1.0,z:1.0},
-    emission_color: Vec3f{x:1.0,y:1.0,z:1.0},
-    transparency: 0.5,
-    reflection: 0.5,
+        center: Vec3f {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        },
+        radius: 2.0,
+        surface_color: Vec3f {
+            x: 1.0,
+            y: 1.0,
+            z: 1.0,
+        },
+        emission_color: Vec3f {
+            x: 1.0,
+            y: 1.0,
+            z: 1.0,
+        },
+        transparency: 0.5,
+        reflection: 0.5,
     };
-    let mut v: Vec<Box<Object>> = Vec::new();
-    v.push(Box::new(t));
-    v.push(Box::new(s));
+    let mut v: Vec<&Object> = Vec::new();
+    // v.push(&t);
+    v.push(&s);
     let ray_origin = Vec3f {
         x: 0.0,
         y: 2.0,
         z: 1.0,
     };
-    let ray_direction = Vec3f {
+    let mut ray_direction = Vec3f {
         x: 0.0,
         y: -2.0,
-<<<<<<< HEAD
         z: 0.0,
     };
     ray_direction.normalize();
@@ -81,25 +103,4 @@ fn main() {
         uv,
         2
     );
-=======
-        z: 2.0,
-    }.normalize();
-    let mut tnear = 1000.0;
-    // let mut normal = Vec3f{
-    //     x:0.0,y:0.0,z:0.0
-    // };
-    // let refract_ray = refract(&ray_direction, &ray_origin , 1.0,1.3);
-
-    // println!(
-    //     "{:?} {:?} {:?},{:?},{:?}",
-    //     t.intersect(&ray_origin, &ray_direction, &mut tnear),
-    //     tnear,
-    //     (7.61 as f64).sqrt(),
-    //     refract_ray,
-    //     (2.0 as f64).sqrt() /2.0 /1.3
-    // );
-    for object in v.iter(){
-        println!("{:?}",object.intersect(&ray_origin, &ray_direction, &mut tnear))
-    }
->>>>>>> 68645430938cd4b43cdc306919896e5c147d22f7
 }
